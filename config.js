d5da81bf4f6bccc80e9ad75c4e2d42716e4a742e
stacksagar <stacksagar@gmail.com>
2023-02-26 18:53:27 +0600

    updated

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-export const api = "https://extract-pdf-data.onrender.com";
+// export const api = "https://extract-pdf-data.onrender.com";
+export const api = "http://localhost:2000";
--- middleware/ExtractImagesFromPDF.js
+++ middleware/ExtractImagesFromPDF.js
@@ -1,27 +1,34 @@
 import { exportImages } from "pdf-export-images";
 import { api } from "../config.js";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default async function ExtractImagesFromPDF(req, res, next) {
-  try {
-    const images = await exportImages(req?.pdfFile, "uploads");
+  req.images = [path.join(__dirname, "uploads")];
+  next();
+  // try {
+  //   const images = await exportImages(req?.pdfFile, "uploads");
 
-    const urls = images.map((image, index) => {
-      const type = index === 0 ? "photo" : "signature";
+  //   const urls = images.map((image, index) => {
+  //     const type = index === 0 ? "photo" : "signature";
 
-      const oldPath = `./uploads/${image.file?.split("\\")[1]}`;
+  //     // const oldPath = path.join(__dirname,`./uploads/${image.file?.split("\\")[1]}` );
+  //     const oldPath = `./uploads/${image.file?.split("\\")[1]}`;
 
-      const newFileName = `${Date.now()}-${type}.png`;
-      const newPath = `./uploads/${newFileName}`;
+  //     const newFileName = `${Date.now()}-${type}.png`;
+  //     const newPath = `./uploads/${newFileName}`;
 
-      fs.renameSync(oldPath, newPath);
+  //     fs.renameSync(oldPath, newPath);
 
-      return api + "/" + newFileName;
-    });
+  //     return api + "/" + newFileName;
+  //   });
 
-    req.images = urls;
-    next();
-  } catch (e) {
-    return res.json({ message: e?.message });
-  }
+  //   req.images = urls;
+  //   next();
+  // } catch (e) {
+  //   return res.json({ message: e?.message });
+  // }
 }
